11ab1ae8c21fc8f8a04336cdb5ca7a8eb3d0eae9
Ubuntu <ubuntu@ip-172-31-65-220.ec2.internal>
2026-04-07 11:27:17 +0000

    Added DynamoDB integration and user APIs

--- handlers/handler.js
+++ handlers/handler.js
@@ -1,3 +1,8 @@
+const { DynamoDBClient, PutItemCommand, ScanCommand } = require("@aws-sdk/client-dynamodb");
+
+const client = new DynamoDBClient({ region: "ap-south-1" });
+
+// GET /
 module.exports.hello = async () => {
   return {
     statusCode: 200,
@@ -5,6 +10,7 @@ module.exports.hello = async () => {
   };
 };
 
+// GET /health
 module.exports.health = async () => {
   return {
     statusCode: 200,
@@ -12,12 +18,34 @@ module.exports.health = async () => {
   };
 };
 
+// GET /users
 module.exports.users = async () => {
+  const data = await client.send(
+    new ScanCommand({
+      TableName: "UsersTable",
+    })
+  );
+
   return {
     statusCode: 200,
-    body: JSON.stringify([
-      { id: 1, name: "Joshua" },
-      { id: 2, name: "DevOps Learner" }
-    ]),
+    body: JSON.stringify(data.Items),
+  };
+};
+
+// POST /users
+module.exports.createUser = async () => {
+  await client.send(
+    new PutItemCommand({
+      TableName: "UsersTable",
+      Item: {
+        id: { S: Date.now().toString() },
+        name: { S: "Joshua" },
+      },
+    })
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "User added successfully" }),
   };
 };
